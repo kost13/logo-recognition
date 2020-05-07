861153d20ee9d71baf7061d72f7533918395c28f
lukasz <l.kostrzewa12@gmail.com>
2020-05-07 13:44:49 +0200

    prepare project structure

---
+++ app/main.cc
@@ -0,0 +1,1 @@
+#include "LogoRecognizer.h"
---
+++ src/LogoRecognizer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace lr {
+    class LogoRecognizer {
+        public:
+        LogoRecognizer() = default;
+
+        private:
+        int a;
+    };
+}
